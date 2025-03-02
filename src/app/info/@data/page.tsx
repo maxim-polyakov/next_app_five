3db98d2f3ed32bd342f229eb644fddb38f98e6d9
maxim-polyakov <maxim7012@mail.ru>
2025-03-02 22:52:14 +0300

    fix

--- src/app/info/@data/page.tsx
+++ src/app/info/@data/page.tsx
@@ -1,9 +1,5 @@
 import { getData } from "@/app/actions/getData"
 
 export default function MapData() {
-    return (
-        <>
-            <text>{getData()}</text>
-        </>
-    )
+    return (<text>{getData()}</text>)
 }
