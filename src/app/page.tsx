ec9d60f0d42f962a3eefd8b04be23772abcad046
maxim-polyakov <maxim7012@mail.ru>
2025-03-03 23:08:47 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 import Image from "next/image";
 import { useState } from "react";
 import { getResponse } from "@/app/actions/getResponse"
+import { getData } from "@/app/actions/getData"
 
 export default function Home() {
     const [value, setValue] = useState('');
+    const val = getData()
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -18,6 +20,8 @@ export default function Home() {
                 />
                 <input type="text" placeholder="Message" name="item_number" required onChange={(e)=>setValue(e.target.value)}/>
                 <button onClick={()=>getResponse({value})}>Send POST</button>
+                <button onClick={()=>setValue(val)}>Get POST</button>
+                <output>{value}</output>
             </main>
         </div>
     );
