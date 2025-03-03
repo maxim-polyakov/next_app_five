f80ee41fb2cdb0eac93e1ab0839e416c8c3f8e8b
maxim-polyakov <maxim7012@mail.ru>
2025-03-03 16:58:44 +0300

    update

--- src/app/actions/getData.ts
+++ src/app/actions/getData.ts
@@ -1,5 +1,5 @@
 export async function getData() {
-    const res = await fetch('http://localhost:3000/api/')
+    const res = await fetch('http://localhost:3000/api/', { next: { revalidate: 0 } })
     const data = await res.json()
     return data.data[0].message.toString()
 }
